1a56f07694bfb7794c895e465f67870c9417646d
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-15 13:19:17 +0100

    Example 01

--- script_01.js
+++ script_01.js
@@ -9,13 +9,15 @@
 
 // Parameter --> funktion getSentence([ ...], "Q"){ ...}
 
-
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-    output(getSentence(["Ich","bin","Max"],"S"));
-    output(getSentence(["Bist","du","Max"],"Q"));
-    output(getSentence(["Ich","bin"],"E"));
-    output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+output(getSentence(["Ich","bin","Max"],"S"));
+output(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Ich","bin"],"E"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+
+
+/*** Example 01  */
 
 // output
     function output( outputStr1 ) { console.log( outputStr1 ); }
@@ -28,23 +30,19 @@
             if ( i < iArr.length - 1 ) {
                 iArrStr += iArr[ i ] + " ";
              } else {
-                iArrStr += iArr[ i ] + ".";
+                iArrStr += iArr[ i ];
              }
          }
          return iArrStr + getMarks( iSng );        
     }
 // get the marks
     function getMarks( iSng ) {
-        let aL = " ";
-        let aS = ".";
-        let aQ = "?";
-        let aE = "!";
         switch ( iSng ) {
             case "Q":
-                return aQ;
+                return "?";
             case "E":
-                return aE;
+                return "!";
             default:
-                return aS;
+                return ".";
         }
     }
